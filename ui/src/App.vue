6facb6db152fa1a9f01f1ed8008eeb9d83742365
Monish Narwani <monish.narwani@focalworks.in>
2022-01-30 16:47:58 +0530

    Added registration & login screen

---
+++ ui/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container mt-5 px-lg-5">
+    <router-link class="nav-link" :to="{ name: 'Login' }"> Login </router-link>
+    <router-link class="nav-link" :to="{ name: 'Register' }">
+      Register
+    </router-link>
+    <router-view />
+  </div>
+</template>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+} */
+
+@import '~bootstrap/dist/css/bootstrap.css';
+</style>
---
+++ ui/src/views/Register.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <section class="vh-100">
+        <div class="container py-5 h-100">
+          <div class="row d-flex align-items-center justify-content-center">
+            <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+              <form>
+                <div class="form-outline mb-4">
+                  <label class="form-label" for="name">Name</label>
+                  <input
+                    type="text"
+                    id="name"
+                    class="form-control form-control-lg"
+                  />
+                </div>
+
+                <div class="form-outline mb-4">
+                  <label class="form-label" for="email">Email address</label>
+                  <input
+                    type="email"
+                    id="email"
+                    class="form-control form-control-lg"
+                  />
+                </div>
+
+                <div class="form-outline mb-4">
+                  <label class="form-label" for="password">Password</label>
+                  <input
+                    type="password"
+                    id="password"
+                    class="form-control form-control-lg"
+                  />
+                </div>
+
+                <div class="form-outline mb-4">
+                  <label class="form-label" for="password_confirm">
+                    Confirm Password
+                  </label>
+                  <input
+                    type="password"
+                    id="password_confirm"
+                    class="form-control form-control-lg"
+                  />
+                </div>
+
+                <div class="d-flex justify-content-around align-items-center">
+                  <a href="#!">New User? Register here..</a>
+                </div>
+
+                <button
+                  type="submit"
+                  class="btn btn-primary btn-lg btn-block m-4"
+                >
+                  Sign in
+                </button>
+
+                <button
+                  type="submit"
+                  class="btn btn-primary btn-lg btn-block m-4"
+                >
+                  Sign in
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
